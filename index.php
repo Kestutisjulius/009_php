28659cf12fe4561f1bd904961ec5155d7aee0c63
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-05-18 05:50:25 +0300

    initial commit2

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title> work</title>
+    </head>
+    <body>
+    <br>
+        <a href="./index.php">index</a>
+        <br>
+        <a href="./index.php">file</a>
+    </body>
+</html>
