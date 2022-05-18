59c5ea572465012551d79f7f7d3f01a0364fc025
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-05-18 06:35:46 +0300

    initial commit3

---
+++ examples/get.php
@@ -0,0 +1,12 @@
+<!-- PHP $_GET
+PHP $_GET is a PHP super global variable which is used to collect form data after submitting an
+HTML form with method="get".
+
+$_GET can also collect data sent in the URL.-->
+<html>
+<body>
+
+<a href="test_get.php?subject=PHP&web=W3schools.com">Test $GET</a>
+
+</body>
+</html>
